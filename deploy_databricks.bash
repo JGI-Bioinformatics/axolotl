a03ee7e3d9f67e291d294780d0fac1e57b04c711
oboezw@gmail.com <zhongwang@lbl.gov>
2022-07-20 16:21:14 -0700

    added databricks deployment script

---
+++ deploy_databricks.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+# deploy build to databricks
+# `./deploy_databricks.bash $version $clusterID`
+# use `databricks clusters list` to find out the clusterID
+# eg. `./deploy_databricks.bash 0.1.0 0719-225420-k5zj97kt`
+
+VERSION=$1
+ClusterID=$2
+# python
+deploydir="dbfs:/FileStore/site_python"
+deploy="sparc-${VERSION}-py3-none-any.whl"
+ 
+rm -rf build/ dist/ axolotl.egg-info/
+python setup.py bdist_wheel
+#pip wheel -r requirements.txt
+databricks fs rm $deploydir/$deploy
+echo "deploying $deploydir/$deploy"
+databricks fs cp dist/$deploy $deploydir/$deploy
+#Install it on a cluster
+databricks libraries uninstall --cluster-id $ClusterID --whl $deploydir/$deploy
+databricks libraries install --cluster-id $ClusterID --whl $deploydir/$deploy
