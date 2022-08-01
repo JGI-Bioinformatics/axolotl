c6809125d200ce10e66ecf32dd16bcc9655183b3
Feng Yu <fyu9@ucmerced.edu>
2022-08-01 12:39:39 -0700

    Update qc.py
    Fixed a bug in the meanQuality query

--- axolotl/qc/qc.py
+++ axolotl/qc/qc.py
@@ -20,7 +20,7 @@ def reads_stats(reads, fraction=1.0, seed=1234, withReplacement=False, show=True
     for col in ['id', 'seq', 'qual']:
         assert (col in reads.columns), col+" column not in reads!" 
 
-    mq_cmd = """SELECT id, name, qual, seq, gc_percent, meanQuality(seq) as mean_qual from reads"""
+    mq_cmd = """SELECT id, name, qual, seq, gc_percent, meanQuality(qual) as mean_qual from reads"""
     gc_cmd = """SELECT id, name, qual, seq, getGC(seq) as gc_percent from reads"""
     if fraction < 1.0:
         reads = reads.sample(withReplacement,fraction,seed)
