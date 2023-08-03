d82fb7438610d71fc37478f2f1f5ee91fb794f11
Satria Kautsar <satriaphd@gmail.com>
2023-08-03 11:33:20 -0700

    added BGC and CDF dataframe class definitions

---
+++ axolotl/data/bgc.py
@@ -0,0 +1,72 @@
+## implement a BGC df generator ##
+
+"""axolotl.data.bgc
+
+Contain classes definition for handling BGCs basic metadata (excluding actual CDSes)
+"""
+
+from pyspark.sql import Row, types, DataFrame
+from typing import List, Dict
+from axolotl.core import ioDF
+from axolotl.data.feature import FeatDF
+
+class bgcDF(ioDF):
+    
+    @classmethod
+    def _getSchemaSpecific(cls) -> types.StructType:
+        return types.StructType([
+            types.StructField("seq_id", types.StringType()),
+            types.StructField("location", types.StructType([
+                types.StructField("start", types.LongType()),
+                types.StructField("end", types.LongType()),
+                types.StructField("strand", types.LongType())
+            ])),
+            types.StructField("classes", types.ArrayType(types.StringType())),
+            types.StructField("on_contig_edge", types.BooleanType()),
+            types.StructField("other_qualifiers", types.ArrayType(
+                types.StructType([
+                    types.StructField("key", types.StringType()),
+                    types.StructField("values", types.ArrayType(types.StringType()))
+                ])
+            ))
+        ])
+        
+    @classmethod
+    def validateRow(cls, row: Row) -> bool:
+        return True
+
+    @classmethod
+    def fromFeatDF(cls, data: FeatDF, source_type: str="antismash"):
+        
+        if source_type == "antismash":
+            df = data.df.filter("type = 'region'")\
+                .rdd.map(lambda row: Row(
+                file_path=row.file_path,
+                row_id=row.row_id,
+                seq_id=row.seq_id,
+                location={
+                    "start": min([loc.start for loc in row.location]),
+                    "end": max([loc.end for loc in row.location]),
+                    "strand": row.location[0].strand if len(set([loc.strand for loc in row.location])) == 1 else 0
+                },
+                classes=([q.values for q in row.qualifiers if q.key == "product"][0:1] or [[]])[0],
+                on_contig_edge=([q.values[0] == "True" for q in row.qualifiers if q.key == "contig_edge"][0:1] or [None])[0],
+                other_qualifiers=[q for q in row.qualifiers if q.key not in ["product", "contig_edge"]]
+            )).toDF(cls.getSchema())
+        elif source_type == "smc":
+            df = data.df.filter("type = 'cluster'")\
+                .rdd.map(lambda row: Row(
+                file_path=row.file_path,
+                row_id=row.row_id,
+                seq_id=row.seq_id,
+                location={
+                    "start": min([loc.start for loc in row.location]),
+                    "end": max([loc.end for loc in row.location]),
+                    "strand": row.location[0].strand if len(set([loc.strand for loc in row.location])) == 1 else 0
+                },
+                classes=([q.values for q in row.qualifiers if q.key == "BGC_Class"][0:1] or [[]])[0],
+                on_contig_edge=([q.values[0] == "True" for q in row.qualifiers if q.key == "contig_edge"][0:1] or [None])[0],
+                other_qualifiers=[q for q in row.qualifiers if q.key not in ["product", "contig_edge"]]
+            )).toDF(cls.getSchema())
+        
+        return cls(df)
---
+++ axolotl/data/cds.py
@@ -0,0 +1,60 @@
+## implement a CDS df generator ##
+
+"""axolotl.data.cds
+
+Contain classes definition for handling coding sequence data
+"""
+
+from pyspark.sql import Row, types, DataFrame
+from axolotl.core import ioDF
+from axolotl.data.feature import FeatDF
+
+
+class cdsDF(ioDF):
+        
+    @classmethod
+    def _getSchemaSpecific(cls) -> types.StructType:
+        return types.StructType([
+            types.StructField("seq_id", types.StringType()),
+            types.StructField("locus_tag", types.StringType()),
+            types.StructField("gene_name", types.StringType()),
+            types.StructField("protein_name", types.StringType()),
+            types.StructField("aa_sequence", types.StringType()),
+            types.StructField("location", types.StructType([
+                types.StructField("start", types.LongType()),
+                types.StructField("end", types.LongType()),
+                types.StructField("strand", types.LongType())
+            ])),
+            types.StructField("other_qualifiers", types.ArrayType(
+                types.StructType([
+                    types.StructField("key", types.StringType()),
+                    types.StructField("values", types.ArrayType(types.StringType()))
+                ])
+            ))
+        ])
+        
+    @classmethod
+    def validateRow(cls, row: Row) -> bool:
+        return row.aa_sequence != None
+
+    @classmethod
+    def fromFeatDF(cls, data: FeatDF):
+        
+        df = data.df.filter("type = 'CDS'")\
+            .rdd.map(lambda row: Row(
+            file_path=row.file_path,
+            row_id=row.row_id,
+            seq_id=row.seq_id,
+            locus_tag=([q.values[0] for q in row.qualifiers if q.key == "locus_tag"][0:1] or [None])[0],
+            gene_name=([q.values[0] for q in row.qualifiers if q.key == "gene"][0:1] or [None])[0],
+            protein_name=([q.values[0] for q in row.qualifiers if q.key == "product"][0:1] or [None])[0],
+            aa_sequence=([q.values[0] for q in row.qualifiers if q.key == "translation"][0:1] or [None])[0],
+            location={
+                "start": min([loc.start for loc in row.location]),
+                "end": max([loc.end for loc in row.location]),
+                "strand": row.location[0].strand if len(set([loc.strand for loc in row.location])) == 1 else 0
+            },
+            other_qualifiers=[q for q in row.qualifiers if q.key not in ["locus_tag", "gene", "product", "translation"]]
+        )).toDF(cls.getSchema())
+        
+        return cls(df)
