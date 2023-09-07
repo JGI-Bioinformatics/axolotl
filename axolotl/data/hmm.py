90d75d3af985b7d25af09b98593504bf12c08d6d
Satria Kautsar <satriaphd@gmail.com>
2023-09-07 18:23:41 -0400

    update hmm scanning

--- axolotl/data/hmm.py
+++ axolotl/data/hmm.py
@@ -14,6 +14,7 @@ from pyspark.sql import Row
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
 
+import warnings
 
 class hmmscanDF(AxlDF):
     
@@ -38,9 +39,9 @@ class hmmscanDF(AxlDF):
         return True
     
     @classmethod
-    def scanCDS(cls, cds_df: cdsDF, hmm_db_path: str):
+    def scanCDS(cls, cds_df: cdsDF, hmm_db_path: str, num_cpus: int=1, T: float=None, E: float=10.0, use_cutoff: str=None):
 
-        def _run_hmmscan(rows: List[Row], hmm_db_path: str, num_cpus: int=1):
+        def _run_hmmscan(rows: List[Row], hmm_db_path: str, num_cpus: int=1, T: float=None, E: float=10.0, use_cutoff: str=None):
             hmm_file = HMMFile(hmm_db_path).optimized_profiles()
             sequences_pyhmmer = (
                 TextSequence(
@@ -49,7 +50,7 @@ class hmmscanDF(AxlDF):
                 ).digitize(Alphabet.amino()) for row in rows if row["aa_sequence"]
             )
             results = []
-            for tophit in pyhmmscan(sequences_pyhmmer, hmm_file, cpus=num_cpus, T=20):
+            for tophit in pyhmmscan(sequences_pyhmmer, hmm_file, cpus=num_cpus, T=T, E=E, bit_cutoffs=use_cutoff):
                 for hit in tophit:
                     results.append({
                         "cds_id": int(tophit.query_name),
@@ -66,8 +67,21 @@ class hmmscanDF(AxlDF):
                     })
             return results
         
+        if use_cutoff:
+            if use_cutoff not in ["NC", "GC" or "TC"]:
+                raise Exception("use an appropriate pHMM cutoff ('NC', 'GC', or 'TC')")
+            use_cutoff = {
+                "GC": "gathering", "TC": "trusted", "NC": "noise"
+            }[use_cutoff]
+        
         return cls(
             cds_df.df.select(["row_id", "aa_sequence"]).rdd.flatMap(
-                lambda row: _run_hmmscan([row], hmm_db_path, 1)
+                lambda row: _run_hmmscan(
+                    [row], hmm_db_path,
+                    num_cpus=num_cpus,
+                    T=T,
+                    E=E,
+                    use_cutoff=use_cutoff
+                )
             ).toDF(cls.getSchema())
         )
