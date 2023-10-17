cf293f2976de04b8b4c01add4a5bac41d423d930
hho38 <hho38@lbl.gov>
2023-10-16 21:48:08 -0700

    add vcf io and data

---
+++ axolotl/data/vcf/vcf.py
@@ -0,0 +1,29 @@
+"""axolotl.data.vcf
+
+Contain classes definition for handing genotype VCF data
+"""
+
+from pyspark.sql import Row, types
+from axolotl.data import ioDF
+
+
+class vcfDF(ioDF):
+        
+    @classmethod
+    def _getSchemaSpecific(cls) -> types.StructType:
+        return types.StructType([
+            types.StructField("chromosome", types.StringType()),
+            types.StructField("position", types.StringType()),
+            types.StructField("ids", types.StringType()),
+            types.StructField("references", types.StringType()),
+            types.StructField("alts", types.StringType()),
+            types.StructField("qual", types.StringType()),
+            types.StructField("filter", types.StringType()),
+            types.StructField("info", types.StringType()),
+            types.StructField("format", types.StringType()),
+            types.StructField("samples", types.ArrayType(types.StringType()))
+        ])
+        
+    @classmethod
+    def validateRow(cls, row: Row) -> bool:
+        return True
