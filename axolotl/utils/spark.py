2ee371d4d611b1a6bdea7bd11970ad32194ffaa9
Satria Kautsar <satriaphd@gmail.com>
2023-12-05 17:07:51 -0500

    get helper function to activate JavaUDF

--- axolotl/utils/spark.py
+++ axolotl/utils/spark.py
@@ -8,3 +8,8 @@ def get_spark_session_and_context():
     sc = spark.sparkContext
 
     return (spark, sc)
+
+
+def activate_udf(udf_name, output_type=None, package_path="org.jgi.axolotl.udfs"):
+    spark, sc = get_spark_session_and_context()
+    spark.udf.registerJavaFunction(udf_name, "{}.{}".format(package_path, udf_name), output_type)
